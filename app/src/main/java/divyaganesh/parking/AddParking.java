04ee591994f2d039ad90b785ba45184f3462c668
Ganesh <itzgannu>
2021-06-01 14:34:16 +0530

    Basic project setup - added empty activities

---
+++ app/src/main/java/divyaganesh/parking/AddParking.java
@@ -0,0 +1,14 @@
+package divyaganesh.parking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddParking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_parking);
+    }
+}
