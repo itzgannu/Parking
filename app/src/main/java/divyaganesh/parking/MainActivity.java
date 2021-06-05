81a23a5bbf2c1f5bd666bf61060abc54a63eb12d
Divya <divya.sundar.287@gmail.com>
2021-06-05 12:51:32 +0530

    Commit changes include : Create Account in Account DB, create login when account is created, changes in naming convention of texts, labels & buttons and string.xml

--- app/src/main/java/divyaganesh/parking/MainActivity.java
+++ app/src/main/java/divyaganesh/parking/MainActivity.java
@@ -2,30 +2,48 @@ package divyaganesh.parking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import divyaganesh.parking.FirebaseData.FirestoreDB;
-import divyaganesh.parking.model.Login;
+import divyaganesh.parking.databinding.ActivityMainBinding;
 import divyaganesh.parking.viewmodels.UsersViewModel;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ActivityMainBinding binding;
     private UsersViewModel usersViewModel;
     private FirestoreDB firestoreDB;
+    private final String TAG = this.getClass().getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         this.usersViewModel = UsersViewModel.getInstance(this.getApplication());
+        this.binding.signInCreateAccBtn.setOnClickListener(this);
+        this.binding.signInBtn.setOnClickListener(this);
     }
 
-    public void OnClick(View view){
+    public void onClick(View view){
         if(view != null){
             switch (view.getId()){
-                case R.id.signIn :
+                case R.id.signInBtn: {
 //                    this.firestoreDB.getExistingUsers();
+                    break;
+                }
+                case R.id.signInCreateAccBtn:{
+                    Log.d(TAG, "onClick: create clicked to redirect");
+                    Intent insertIntent = new Intent(this, CreateAccount.class);
+                    startActivity(insertIntent);
+                    break;
+                }
             }
         }
     }
